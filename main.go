1da520204f62dabbb0f89f084804feb0817d6ea5
Ludvig Kirkeby <luki@itu.dk>
2025-11-06 23:37:51 +0100

    Almost functioning, issue with int64 vs int type

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	proto "Ricart_and_Agrawala_implementation/grpc" // adjust to your generated package path
+	"context"
+	"log"
+
 	//"context"
 	//"log"
 	"fmt"
@@ -14,28 +17,25 @@ import (
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
-type message struct {
-	timestamp      int64
-	answer_channel chan message
-}
-
 // used for each server for each node
 // this is a server
 type Node struct {
 	proto.UnimplementedNodeServiceServer
+
 	mutex     sync.Mutex // lock for lamport stuff
 	state     int
 	timestamp int64
 	id        int
 	port      int
-	node_map  map[int]proto.NodeServiceClient
-	queue     []chan message
+
+	node_map map[int]proto.NodeService_SendClient
+	queue    []proto.NodeService_SendServer // queue of streams to reply to
 }
 
 var node_count = 5
+var start_port = 8080
 
 func main() {
-	var start_port = 8080
 	for i := start_port; i < node_count+start_port; i++ {
 		new_node := Node{
 			timestamp: 0,
@@ -47,78 +47,82 @@ func main() {
 		}
 		go new_node.run()
 	}
-	for {
-
-	}
 }
 
 func (n *Node) run() {
-	// determine state
-	// if state is...
-	go n.receive()
+	n.startServer(n.port)
+	// POSSIBLY ISSUE: Connecting before all servers are started, can be tested with logging
+	for i := start_port; i < node_count+start_port; i++ {
+		if n.port == i {
+			continue
+		}
+		n.connect(i)
+	}
+
 	n.state = 0
 
-	for {
-		if n.state == 2 {
-			n.setLeader()
-		}
-		n.enter()
+	if n.state == 2 {
+		n.setLeader()
 	}
+
+	n.enter()
+
 }
 
 func (n *Node) enter() {
-	// set state to 1, for wanted
+	// set state to 1 for wanted and start sending requests
 	n.state = 1
-	n.send()
+	n.sendToAll()
 }
 
-func (n *Node) send() {
+func (n *Node) sendToAll() {
 
-	msg := message{
-		timestamp: n.timestamp,
-		port:      n.port,
+	msg := &proto.Request{
+		Timestamp: n.timestamp,
+		Port:      n.port,
 	}
 
-	for _, c := range n.node_list {
-		fmt.Printf("Sending message %v from %v to %v \n", msg.timestamp, n.id, p.id)
-		// send msg to all other nodes
-		c.send(msg)
+	// sends message to all other nodes
+	for port, stream := range n.node_map {
+		fmt.Printf("Sending request from port [%d] to port [%d]", n.port, port)
+
+		if err := stream.Send(msg); err != nil {
+			// log failed to send
+		}
 
 		n.mutex.Lock()
 		n.timestamp++
 		n.mutex.Unlock()
 	}
 
-	for _, p := range nodes {
-		if p.id == n.id {
-			continue
-		}
+	// wait to receive all "yes"
+	replyCh := make(chan *proto.Reply, len(n.node_map))
 
+	for port, stream := range n.node_map {
+		go func(p int, s proto.NodeService_SendClient) {
+			rep, err := s.Recv() // block until a reply comes
+			if err != nil {
+				log.Printf("[node %d] error receiving from %d: %v", n.port, p, err)
+				return
+			}
+			replyCh <- rep
+		}(port, stream) // ??? Kinda confused
 	}
 
-	// wait to receive all "yes"#
-	for i := 1; i < node_count; i++ {
-		fmt.Printf("%v received %d yes \n", n.id, i)
-		<-n.answer_channel
-	}
-	// Now has token
-	n.state = 2
-}
-
-func (n *Node) receive() {
-	for {
-		rec_msg := <-n.req_channel
-		if n.state == 2 || n.state == 1 && rec_msg.timestamp > n.timestamp {
-			n.queue = append(n.queue, rec_msg.answer_channel)
-		} else {
-			rec_msg.answer_channel <- message{0, nil}
-		}
+	// wait for replies from all peers (exclude self if present in node_map)
+	expectedReplies := len(n.node_map)
+	received := 0
+	for received < expectedReplies {
+		<-replyCh
+		received++
 	}
 }
 
+// This is deceiving: its called "server stream" but what it is, is the stream that was used to send
+// the request that we queued.
 func (n *Node) exit() {
-	for _, c := range n.queue {
-		c <- message{0, nil}
+	for _, server_stream := range n.queue {
+		server_stream.Send(&proto.Reply{}) // gets thrown out immediately,so can be blank
 	}
 	n.queue = n.queue[:0]
 }
@@ -130,9 +134,8 @@ func (n *Node) setLeader() {
 	n.exit()
 }
 
-// / Server stuff
 // Should start server for a different address each time (8080, 8081...) one for each node
-// Each "Node" is a server itself (As it is here it receives requests)
+// Server only handles receive and reply
 func (n *Node) startServer(port int) {
 	addr := fmt.Sprintf(":%d", port) // so its not hardcoded anymore
 	listener, err := net.Listen("tcp", addr)
@@ -146,16 +149,20 @@ func (n *Node) startServer(port int) {
 	// Registers the grpc server with the System struct
 	proto.RegisterNodeServiceServer(grpc_server, n)
 
+	// this blocks ???
 	err = grpc_server.Serve(listener)
 	if err != nil {
 		// logging
 	}
-
 	// logging like, "server started at... for node..."
 }
 
 // connects to another client with their port
 func (n *Node) connect(port int) {
+	if port == n.port {
+		return
+	} // Make sure we don't connect to our selves
+
 	addr := fmt.Sprintf("localhost:%d", port) // so its not hardcoded anymore
 	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -163,13 +170,33 @@ func (n *Node) connect(port int) {
 	}
 	defer conn.Close() // keeps connnection open
 
-	// A mapping between port to an open connection on it. This is used to get a received port from send
-	n.node_map[port] = proto.NewNodeServiceClient(conn)
+	client := proto.NewNodeServiceClient(conn)
 
-	// Below is an example of how to call using the connection
-	//client := proto.NewNodeServiceClient(conn)
-	// message, err := client.Receive(context.Background())
-	// if err != nil {
-	// logging
-	// }
+	// Create a bidirectional stream
+	stream, err := client.Send(context.Background())
+	if err != nil {
+		// logging
+		return
+	}
+
+	n.node_map[port] = stream // map port to open stream
+}
+
+func (n *Node) Send(stream proto.NodeService_SendServer) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+
+		// timestamp stuff + lock
+		if n.state == 2 || n.state == 1 && req.Timestamp > n.timestamp {
+			n.queue = append(n.queue, stream) // we save the stream of the sender for later use
+		} else {
+			reply := &proto.Reply{}                    // can be blank, gets thrown out immediately anyway
+			if err := stream.Send(reply); err != nil { // we reply directly on sender stream
+				return err
+			}
+		}
+	}
 }
